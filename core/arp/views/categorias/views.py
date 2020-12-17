f500a15c30ab36b0da60991d8961a572ad64039f
KenethSandoval <kenethra.74@gmail.com>
2020-12-17 12:20:18 -0600

    [feat]: category list success

--- core/arp/views/categorias/views.py
+++ core/arp/views/categorias/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from core.arp.models import Category
 
 def category_list(request):
     data = {
-
-           }
+        'title': 'Listado de categorias',
+        'categories': Category.objects.all()
+    }
     return render(request, 'category/list.html', data)

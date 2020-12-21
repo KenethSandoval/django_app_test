66579b6f990f25ce7277336d053ae3e70325226e
KenethSandoval <kenethra.74@gmail.com>
2020-12-21 12:50:29 -0600

    [feat]: use of ajax for post method

--- core/arp/views/categorias/views.py
+++ core/arp/views/categorias/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from core.arp.models import Category
 from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
 
 class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
-    
+  
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+         
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+        
+        return JsonResponse(data)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Listado de Categorías'
